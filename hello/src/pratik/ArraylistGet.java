c16cbe5acd75abd3c9507029e03f4a3493556f3f
pratik29046 <79098295+pratik29046@users.noreply.github.com>
2022-04-18 17:47:42 +0530

    Create ArraylistGet.java

---
+++ hello/src/pratik/ArraylistGet.java
@@ -0,0 +1,19 @@
+package pratik;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] a = new String[1000];
+		a[0] = "Avantika";
+		a[1] = "Arayan";
+		ArrayList<String> list1 = new ArrayList<String>();
+		list1.add("pratk");
+    list1.add("ram");
+		String name = (String) l.get(0);
+		System.out.println(name);
+	}
+
+}
