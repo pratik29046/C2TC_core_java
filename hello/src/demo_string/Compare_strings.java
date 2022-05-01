f9193b6c35ef541049824f7eb0d7801cb3b5fefa
pratik29046 <79098295+pratik29046@users.noreply.github.com>
2022-05-01 15:11:29 +0530

    first commit

---
+++ hello/src/demo_string/Compare_strings.java
@@ -0,0 +1,11 @@
+package demo_string;
+
+public class Compere_strings  {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		String s1 = new String("Hello");
+		String s2 = new String("Hellow");
+		System.out.println(s1 = s2);
+	}
+
