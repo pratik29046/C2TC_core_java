d61bfebfe892caddbbe9947429ab270b5668336e
pratik29046 <79098295+pratik29046@users.noreply.github.com>
2022-05-01 15:14:27 +0530

    first commit

---
+++ hello/src/demo_string/Equal.java
@@ -0,0 +1,12 @@
+package demo_string;
+
+public class Equal {
+
+	public static void main(String[] args) {
+		String s1 = "java";
+		String s2 = "java";
+		System.out.println(s1.equals(s2));
+		System.out.println(s1 == s2);
+	}
+
+}
