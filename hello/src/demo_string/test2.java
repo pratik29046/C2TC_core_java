ff8a76dd71af0c5ba8dc46d081cc14eb403328ed
pratik29046 <79098295+pratik29046@users.noreply.github.com>
2022-05-01 15:13:15 +0530

    first commit

---
+++ hello/src/demo_string/test2.java
@@ -0,0 +1,12 @@
+package demo_string;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		String s1 = new String("OKAY");
+		String s2 = new String(s1);
+		System.out.println(s1 == s2);
+		
+	}
+
+}
